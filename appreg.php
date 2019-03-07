c078463409e63037fc15be2ba39e80571021f55f
hawkinslauren <45079093+hawkinslauren@users.noreply.github.com>
2019-03-07 10:43:39 -0500

    Add files via upload

---
+++ appreg.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<title>OBT Cohort Database</title>
+	<style>
+		/*custom page css here*/
+	</style>
+</head>
+<body>
+<!--	<?php require_once('assets/partials/nav.php')?>-->
+	<!-- HTML here. -->
+    <center><h1>OBT Cohort Database</h1></center>
+    <center><p><a href="index.html" class="btn-primary btn">Back to Home</a></p></center>
+	<center><div class="container">
+		<h2>Registration</h2>
+		<form action="assets/datahandler/regprocess.php" method="post" enctype="multipart/form-data">
+			<label>Email <br>
+				<input type="text" name="email">
+			</label>
+			<br>
+			<label>Password <br>
+				<input type="password" name="pw">
+			</label>
+			<br> 
+			<br>
+			<input type="submit" value="Register">
+		</form>
+        </div></center>
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="assets/js/jquery.js"></script>
+	<script src="assets/js/popper.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script>
+		/* custom script here */
+	</script>
+</body>
+</html>
