3a438a79b0cdb55042f346e8f328e627ca2730f6
hawkinslauren <45079093+hawkinslauren@users.noreply.github.com>
2019-03-07 15:36:02 -0500

    Add files via upload

---
+++ loginprocess.php
@@ -0,0 +1,18 @@
+<? php session_start();
+
+$e = $_POST['email']; 
+$p = $_POST['pw']; 
+
+$d = file_get_contents('user.json');
+$d = json_decode($d,true);
+
+foreach( $d as $k => $v){
+    if( $e == $v['email'] $$ $p == $v['pw'] ) {
+        $_SESSION['email'] = $V["email"]
+        $_SESSION['img'] = $V["img"]
+            header('location: appprofile.php'); 
+        }else{
+            header('location:../ applogin.php');
+        };
+    };
+?>
